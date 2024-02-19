b521c476e21e628dfebc6ade4e9d668e7597c0a3
nick v <nvalente.electrics@gmail.com>
2024-02-19 14:51:52 +0000

    added more lessons on conditionals

---
+++ C#_conditional_statements/if_else.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace IfElseStatement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int people = 14;
+      string weather = "nice";
+      if(people <= 10 && weather == "nice"){
+        Console.WriteLine("SaladMart");
+      } else {
+        Console.WriteLine("Soup N Sandwich")
+      }
+
+    }
+  }
+}
