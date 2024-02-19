d52885494ba9ecea0ebfed747bb4ac9ef61b9bcb
nick v <nvalente.electrics@gmail.com>
2024-02-19 14:53:40 +0000

    added more lessons on conditionals

---
+++ C#_conditional_statements/review_practice_file.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+namespace Review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* use this space to write your own short program! 
+      Here's what you learned:
+
+      CONDITIONALS: if, if...else, else if
+      SWITCH STATEMENT: switch (condition)
+      TERNARY OPERATOR: (condition) ? true : false
+
+      Good luck! */
+
+
+    }
+  }
+}
